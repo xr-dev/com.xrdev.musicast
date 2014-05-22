8c1ce5102616cb5dbc167caccec8acb29171c740
Guilherme Barreto <gsb.barreto@gmail.com>
2014-05-22 20:43:26 -0300

    Libs do YouTube corrigidas.

---
+++ Musicast/src/com/xrdev/musicast/connection/YouTubeHandler.java
@@ -0,0 +1,10 @@
+package com.xrdev.musicast.connection;
+
+import com.google.api.services.youtube.YouTube;
+
+
+public class YouTubeHandler {
+	private static final long NUMBER_OF_VIDEOS_RETURNED = 15;
+	private static YouTube youtube;
+	
+}
